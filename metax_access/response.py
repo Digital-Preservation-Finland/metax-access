bd2ac3836d4e26de1e2d0fe20001dbe889377555
Joel Weinitschke <joel.weinitschke@csc.fi>
2025-08-21 17:21:49 +0300

    KDKPAS-3508 Refactor response mapper functions
    - Remove nested conditional expressions
    - Divide dictionary creation to multiple statements
    - Update response module TypedDicts

--- metax_access/response.py
+++ metax_access/response.py
@@ -10,7 +10,8 @@ For underlying data models, see:
 
 https://metax-v3.fd-staging.csc.fi/v3/swagger/
 """
-from typing import TypedDict, Optional, Union, TYPE_CHECKING
+from collections.abc import Sequence
+from typing import Any, TypedDict, Optional, Union, TYPE_CHECKING
 
 if TYPE_CHECKING:
     # Type checkers need to support NotRequired
@@ -20,9 +21,11 @@ else:
     Required = Union
 
 
-class MetaxPrefLabel(TypedDict):
-    en: str
-    fi: str
+class MetaxPrefLabel(TypedDict, total=False):
+    en: Optional[str]
+    fi: Optional[str]
+    sv: Optional[str]
+    und: Optional[str]
 
 
 class MetaxFileDatasetMetadataEntry(TypedDict, total=False):
@@ -79,3 +82,208 @@ class MetaxFile(TypedDict, total=False):
     # TODO: Temporary field used during V2-V3 transition. Metax V3 has no
     # file creation date field, but this is still required for V2.
     _file_uploaded: Optional[str]
+
+
+class MetaxMinFile(TypedDict, total=False):
+    id: Required[str]
+    filename: Required[str]
+    size: Required[str]
+
+
+class MetaxParentDirectory(TypedDict, total=False):
+    storage_service: Optional[str]
+    csc_project: Optional[str]
+    name: Optional[str]
+    pathname: Required[str]
+    file_count: Optional[int]
+    size: Optional[int]
+    created: Optional[str]
+    modified: Optional[str]
+    dataset_metadata: Optional[Any]
+    parent_url: Optional[str]
+
+
+class MetaxDirectory(TypedDict, total=False):
+    storage_service: Optional[str]
+    csc_project: Optional[str]
+    name: Required[str]
+    pathname: Required[str]
+    file_count: Required[int]
+    size: Required[int]
+    created: Optional[str]
+    modified: Optional[str]
+    dataset_metadata: Optional[Any]
+    url: Optional[str]
+
+
+class MetaxDirectoryFiles(TypedDict):
+    directory: Optional[MetaxParentDirectory]
+    files: Sequence[MetaxMinFile]
+    directories: Sequence[MetaxDirectory]
+
+
+class MetaxContractOrganization(TypedDict):
+    name: str
+    organization_identifier: str
+
+
+class DateRange(TypedDict, total=False):
+    start_date: Required[str]
+    end_date: Optional[str]
+    temporal_coverage: Optional[Any]
+
+
+class MetaxContact(TypedDict):
+    name: str
+    email: str
+    phone: str
+
+
+class RelatedService(TypedDict):
+    identifier: str
+    name: str
+
+
+class MetaxDataFieldBase(TypedDict):
+    pref_label: MetaxPrefLabel
+
+
+class MetaxDataField(MetaxDataFieldBase, TypedDict, total=False):
+    id: Optional[str]
+    in_scheme: Optional[str]
+    url: Required[Optional[str]]
+
+
+class MetaxUrlField(TypedDict):
+    url: str
+
+
+class MetaxContract(TypedDict, total=False):
+    id: Required[str]
+    title: Required[MetaxPrefLabel]
+    description: Required[MetaxPrefLabel]
+    quota: Required[int]
+    created: Required[str]
+    modified: Optional[str]
+    organization: Required[MetaxContractOrganization]
+    validity: Required[DateRange]
+    contact: Required[Sequence[MetaxContact]]
+    related_service: Required[Sequence[RelatedService]]
+    removed: Optional[bool]
+
+
+class MetaxLicense(TypedDict, total=False):
+    id: Optional[str]
+    pref_label: Required[Optional[MetaxPrefLabel]]
+    custom_url: Optional[str]
+    description: Optional[MetaxPrefLabel]
+    title: Optional[MetaxPrefLabel]
+    url: Required[str]
+
+
+class MetaxAccessRights(TypedDict, total=False):
+    available: Optional[str]
+    description: Optional[MetaxPrefLabel]
+    license: Required[Sequence[MetaxLicense]]
+
+
+class MetaxPerson(TypedDict, total=False):
+    name: Required[str]
+    external_identifier: Required[Optional[str]]
+    email: Optional[str]
+
+
+class MetaxOrganization(TypedDict):
+    pref_label: MetaxPrefLabel
+    url: str
+    external_identifier: str
+    parent: Optional[MetaxDataFieldBase]
+
+
+class MetaxActor(TypedDict, total=False):
+    roles: Sequence[str]
+    person: Optional[MetaxPerson]
+    organization: Optional[MetaxOrganization]
+
+
+class MetaxFileSet(TypedDict):
+    csc_project: Optional[str]
+    total_files_size: Required[int]
+    total_files_count: Required[int]
+
+
+class MetaxMetadataOwner(TypedDict, total=False):
+    id: Optional[str]
+    user: Required[str]
+    organization: Required[str]
+
+
+class MetaxPreservationDatasetVersion(TypedDict):
+    id: Optional[str]
+    persistent_identifier: Optional[str]
+    preservation_state: Optional[str]
+
+
+class MetaxPreservation(TypedDict, total=False):
+    state: Required[int]
+    description: Required[Optional[MetaxPrefLabel]]
+    reason_description: Required[Optional[MetaxPrefLabel]]
+    dataset_version: Optional[MetaxPreservationDatasetVersion]
+    contract: Required[Optional[str]]
+
+
+class MetaxProvenance(TypedDict, total=False):
+    title: Required[MetaxPrefLabel]
+    temporal: Optional[DateRange]
+    description: Required[MetaxPrefLabel]
+    event_outcome: Optional[MetaxDataField]
+    outcome_description: Required[MetaxPrefLabel]
+    lifecycle_event: Optional[MetaxDataFieldBase]
+
+
+class MetaxSpatial(TypedDict):
+    geographic_name: str
+
+
+class MetaxDataset(TypedDict, total=False):
+    id: Required[str]
+    access_rights: Required[Optional[MetaxAccessRights]]
+    actors: Required[Sequence[MetaxActor]]
+    bibliographic_citations: Optional[Any]
+    cumulative_state: Optional[int]
+    data_catalog: Required[Optional[str]]
+    description: Required[Optional[MetaxPrefLabel]]
+    field_of_science: Required[Sequence[MetaxDataFieldBase]]
+    fileset: Required[MetaxFileSet]
+    generate_pid_on_publish: Optional[str]
+    infrastructure: Optional[Sequence[Any]]
+    issued: Required[Optional[str]]
+    keyword: Required[Sequence[str]]
+    language: Required[Sequence[MetaxUrlField]]
+    metadata_owner: Required[MetaxMetadataOwner]
+    other_identifiers: Optional[Sequence[Any]]
+    persistent_identifier: Required[Optional[str]]
+    preservation: Required[MetaxPreservation]
+    projects: Optional[Sequence[Any]]
+    provenance: Required[Sequence[MetaxProvenance]]
+    relation: Optional[Sequence[Any]]
+    remote_resources: Optional[Sequence[Any]]
+    spatial: Required[Sequence[MetaxSpatial]]
+    state: Optional[str]
+    temporal: Optional[Sequence[DateRange]]
+    theme: Required[Sequence[MetaxDataFieldBase]]
+    title: Required[MetaxPrefLabel]
+    created: Required[str]
+    cumulation_started: Optional[str]
+    cumulation_ended: Optional[str]
+    deprecated: Optional[str]
+    removed: Optional[str]
+    modified: Required[str]
+    dataset_versions: Optional[Sequence["MetaxDataset"]]
+    published_revision: Optional[int]
+    draft_revision: Optional[int]
+    draft_of: Optional[str]
+    next_draft: Optional[str]
+    version: Required[int]
+    api_version: Optional[int]
+    metadata_repository: Optional[str]
--- metax_access/response_mapper.py
+++ metax_access/response_mapper.py
@@ -1,24 +1,50 @@
 """Mapper for Metax responses."""
 
+from __future__ import annotations
+from typing import Sequence
 
-def map_directory_files(metax_directory_files):
+from metax_access.response import (
+    MetaxAccessRights,
+    MetaxActor,
+    MetaxContract,
+    MetaxDataFieldBase,
+    MetaxDataset,
+    MetaxDirectoryFiles,
+    MetaxFile,
+    MetaxFileCharacteristics,
+    MetaxFileDatasetMetadata,
+    MetaxFileDatasetMetadataEntry,
+    MetaxFileSet,
+    MetaxMetadataOwner,
+    MetaxMinFile,
+    MetaxParentDirectory,
+    MetaxPreservation,
+    MetaxProvenance,
+)
+
+
+def map_directory_files(
+    metax_directory_files: MetaxDirectoryFiles,
+) -> MetaxDirectoryFiles:
     """Maps a metax directory file response to a minimum response
     required by the FDPAS services.
     """
+    files: list[MetaxMinFile] = [
+        {
+            "id": file["id"],
+            "filename": file["filename"],
+            "size": file["size"],
+        }
+        for file in metax_directory_files["files"]
+    ]
+
+    parent_directory: MetaxParentDirectory | None = None
+    if (input_directory := metax_directory_files.get("directory")) is not None:
+        parent_directory = {"pathname": input_directory["pathname"]}
+
     return {
-        "directory": (
-            {"pathname": metax_directory_files["directory"]["pathname"]}
-            if metax_directory_files["directory"] is not None
-            else None
-        ),
-        "files": [
-            {
-                "id": file["id"],
-                "filename": file["filename"],
-                "size": file["size"],
-            }
-            for file in metax_directory_files["files"]
-        ],
+        "directory": parent_directory,
+        "files": files,
         "directories": [
             {
                 "name": directory["name"],
@@ -31,10 +57,53 @@ def map_directory_files(metax_directory_files):
     }
 
 
-def map_file(metax_file):
+def map_file(metax_file: MetaxFile) -> MetaxFile:
     """Maps a Metax file response to a minimum response
     required by the FDPAS services.
     """
+
+    use_category: MetaxFileDatasetMetadataEntry | None = None
+
+    # dataset metadata field is only included when file is queried
+    # related to the dataset. e.g. get dataset files method. Otherwise
+    # Metax won't include it to the response.
+    if (input_data := metax_file.get("dataset_metadata")) is not None:
+        if (input_use := input_data.get("use_category")) is not None:
+            use_category = {
+                "pref_label": input_use.get("pref_label"),
+                "url": input_use.get("url"),
+            }
+
+    dataset_metadata: MetaxFileDatasetMetadata = {"use_category": use_category}
+
+    characteristics: MetaxFileCharacteristics = {
+        "file_format_version": {
+            "file_format": None,
+            "format_version": None,
+        },
+        "encoding": None,
+        "csv_delimiter": None,
+        "csv_record_separator": None,
+        "csv_quoting_char": None,
+        "csv_has_header": None,
+    }
+
+    if (input_char := metax_file.get("characteristics")) is not None:
+        if (input_format := input_char.get("file_format_version")) is not None:
+            characteristics["file_format_version"] = {
+                "file_format": input_format.get("file_format"),
+                "format_version": input_format.get("format_version"),
+            }
+
+        for key in [
+            "encoding",
+            "csv_delimiter",
+            "csv_record_separator",
+            "csv_quoting_char",
+            "csv_has_header",
+        ]:
+            characteristics[key] = input_char.get(key)
+
     return {
         "id": metax_file["id"],
         "storage_identifier": metax_file["storage_identifier"],
@@ -44,86 +113,17 @@ def map_file(metax_file):
         "checksum": metax_file["checksum"],
         "csc_project": metax_file["csc_project"],
         "storage_service": metax_file["storage_service"],
-        # dataset metadata field is only included when file is queried
-        # related to the dataset. e.g. get dataset files method. Otherwise
-        # Metax won't include it to the response.
-        "dataset_metadata": (
-            {
-                "use_category": (
-                    {
-                        "pref_label": (
-                            metax_file["dataset_metadata"]["use_category"][
-                                "pref_label"
-                            ]
-                        ),
-                        "url": metax_file["dataset_metadata"]["use_category"][
-                            "url"
-                        ],
-                    }
-                    if metax_file.get("dataset_metadata", {}).get(
-                        "use_category"
-                    )
-                    is not None
-                    else None
-                )
-            }
-            if metax_file.get("dataset_metadata") is not None
-            else {"use_category": None}
+        "dataset_metadata": dataset_metadata,
+        "characteristics": characteristics,
+        "characteristics_extension": metax_file.get(
+            "characteristics_extension"
         ),
-        "characteristics": (
-            {
-                "file_format_version": (
-                    {
-                        "file_format": metax_file["characteristics"][
-                            "file_format_version"
-                        ]["file_format"],
-                        "format_version": metax_file["characteristics"][
-                            "file_format_version"
-                        ]["format_version"],
-                    }
-                    if metax_file["characteristics"]["file_format_version"]
-                    is not None
-                    else {
-                        "file_format": None,
-                        "format_version": None,
-                    }
-                ),
-                "encoding": metax_file["characteristics"]["encoding"],
-                "csv_delimiter": metax_file["characteristics"][
-                    "csv_delimiter"
-                ],
-                "csv_record_separator": metax_file["characteristics"][
-                    "csv_record_separator"
-                ],
-                "csv_quoting_char": metax_file["characteristics"][
-                    "csv_quoting_char"
-                ],
-                "csv_has_header": metax_file["characteristics"][
-                    "csv_has_header"
-                ],
-            }
-            if metax_file["characteristics"] is not None
-            else (
-                {
-                    "file_format_version": {
-                        "file_format": None,
-                        "format_version": None,
-                    },
-                    "encoding": None,
-                    "csv_delimiter": None,
-                    "csv_record_separator": None,
-                    "csv_quoting_char": None,
-                    "csv_has_header": None,
-                }
-            )
-        ),
-        "characteristics_extension": metax_file["characteristics_extension"],
-        "pas_compatible_file": metax_file["pas_compatible_file"],
-        "non_pas_compatible_file": metax_file["non_pas_compatible_file"],
+        "pas_compatible_file": metax_file.get("pas_compatible_file"),
+        "non_pas_compatible_file": metax_file.get("non_pas_compatible_file"),
     }
 
 
-def map_contract(metax_contract):
+def map_contract(metax_contract: MetaxContract) -> MetaxContract:
     """Maps a Metax contract response to a minimum response
     required by the FDPAS services.
     """
@@ -144,90 +144,109 @@ def map_contract(metax_contract):
     }
 
 
-def map_dataset(metax_dataset):
+def map_dataset(metax_dataset: MetaxDataset) -> MetaxDataset:
     """Maps a Metax dataset response to a minimum response
     required by the FDPAS services.
     """
+
+    fileset: MetaxFileSet = (
+        {
+            "total_files_size": metax_dataset["fileset"]["total_files_size"],
+            "csc_project": metax_dataset["fileset"]["csc_project"],
+            "total_files_count": metax_dataset["fileset"]["total_files_count"],
+        }
+        if metax_dataset["fileset"]
+        else {
+            "total_files_size": 0,
+            "csc_project": None,
+            "total_files_count": 0,
+        }
+    )
+
+    preservation: MetaxPreservation = {
+        "state": -1,
+        "description": None,
+        "reason_description": None,
+        "dataset_version": {
+            "id": None,
+            "persistent_identifier": None,
+            "preservation_state": None,
+        },
+        "contract": None,
+    }
+
+    if (input_pres := metax_dataset["preservation"]) is not None:
+        preservation["state"] = input_pres["state"]
+        preservation["description"] = input_pres["description"]
+        preservation["reason_description"] = input_pres["reason_description"]
+        preservation["contract"] = input_pres["contract"]
+
+        if (dataset_ver := input_pres.get("dataset_version")) is not None:
+            preservation["dataset_version"] = {
+                "id": dataset_ver["id"],
+                "persistent_identifier": dataset_ver["persistent_identifier"],
+                "preservation_state": dataset_ver["preservation_state"],
+            }
+
+    access_rights: MetaxAccessRights | None = None
+
+    if metax_dataset["access_rights"] is not None:
+        access_rights = {
+            "license": [
+                {
+                    "url": license["url"] or "",
+                    "pref_label": license["pref_label"],
+                }
+                for license in metax_dataset["access_rights"]["license"]
+            ]
+        }
+
+    def _map_provenance(member: MetaxProvenance) -> MetaxProvenance:
+        p: MetaxProvenance = {
+            "title": member["title"],
+            "description": member["description"],
+            "outcome_description": member["outcome_description"],
+            "temporal": None,
+            "event_outcome": None,
+            "lifecycle_event": None,
+        }
+
+        if (temp := member.get("temporal")) is not None:
+            p["temporal"] = {"start_date": temp["start_date"]}
+
+        if (outcome := member.get("event_outcome")) is not None:
+            p["event_outcome"] = {
+                "pref_label": outcome["pref_label"],
+                "url": outcome["url"],
+            }
+
+        if (event := member.get("lifecycle_event")) is not None:
+            p["lifecycle_event"] = {"pref_label": event["pref_label"]}
+
+        return p
+
+    provenance: list[MetaxProvenance] = [
+        _map_provenance(member) for member in metax_dataset["provenance"]
+    ]
+
+    metadata_owner: MetaxMetadataOwner = {
+        # TODO: `metadata_owner.user` and
+        # `metadata_owner.organization` might be hidden in some
+        # datasets, so they then set to `None`. Does it make
+        # sense fetch those datasets? Can it be avoided?
+        "organization": (metax_dataset["metadata_owner"].get("organization")),
+        "user": metax_dataset["metadata_owner"].get("user"),
+    }
+
     return {
         "id": metax_dataset["id"],
         "created": metax_dataset["created"],
         "title": metax_dataset["title"],
         "description": metax_dataset["description"],
         "modified": metax_dataset["modified"],
-        "fileset": (
-            {
-                "total_files_size": metax_dataset["fileset"][
-                    "total_files_size"
-                ],
-                "csc_project": metax_dataset["fileset"]["csc_project"],
-                "total_files_count": metax_dataset["fileset"][
-                    "total_files_count"
-                ],
-            }
-            if metax_dataset["fileset"]
-            else {
-                "total_files_size": 0,
-                "csc_project": None,
-                "total_files_count": 0,
-            }
-        ),
-        "preservation": (
-            {
-                "state": metax_dataset["preservation"]["state"],
-                "description": metax_dataset["preservation"]["description"],
-                "reason_description": metax_dataset["preservation"][
-                    "reason_description"
-                ],
-                # This field is created when a dataset is preserved
-                "dataset_version": (
-                    {
-                        "id": metax_dataset["preservation"]["dataset_version"][
-                            "id"
-                        ],
-                        "persistent_identifier": metax_dataset["preservation"][
-                            "dataset_version"
-                        ]["persistent_identifier"],
-                        "preservation_state": metax_dataset["preservation"][
-                            "dataset_version"
-                        ]["preservation_state"],
-                    }
-                    if "dataset_version" in metax_dataset["preservation"]
-                    and metax_dataset["preservation"]["dataset_version"]
-                    is not None
-                    else {
-                        "id": None,
-                        "persistent_identifier": None,
-                        "preservation_state": None,
-                    }
-                ),
-                "contract": metax_dataset["preservation"]["contract"],
-            }
-            if metax_dataset["preservation"] is not None
-            else {
-                "state": -1,
-                "description": None,
-                "reason_description": None,
-                "dataset_version": {
-                    "id": None,
-                    "persistent_identifier": None,
-                    "preservation_state": None,
-                },
-                "contract": None,
-            }
-        ),
-        "access_rights": (
-            {
-                "license": [
-                    {
-                        "url": license["url"] or "",
-                        "pref_label": license["pref_label"],
-                    }
-                    for license in metax_dataset["access_rights"]["license"]
-                ]
-            }
-            if metax_dataset["access_rights"] is not None
-            else None
-        ),
+        "fileset": fileset,
+        "preservation": preservation,
+        "access_rights": access_rights,
         "version": metax_dataset["version"],
         "language": [
             {"url": language["url"]} for language in metax_dataset["language"]
@@ -248,89 +267,44 @@ def map_dataset(metax_dataset):
             {"pref_label": field["pref_label"]}
             for field in metax_dataset["field_of_science"]
         ],
-        "provenance": [
-            {
-                "title": p["title"],
-                "temporal": (
-                    {
-                        "start_date": p["temporal"]["start_date"]
-                    }
-                    if p["temporal"] is not None
-                    else None
-                ),
-                "description": p["description"],
-                "event_outcome": (
-                    {
-                        "pref_label": p["event_outcome"]["pref_label"],
-                        "url": p["event_outcome"]["url"],
-                    }
-                    if p["event_outcome"] is not None
-                    else None
-                ),
-                "outcome_description": p["outcome_description"],
-                "lifecycle_event": (
-                    {"pref_label": p["lifecycle_event"]["pref_label"]}
-                    if p["lifecycle_event"] is not None
-                    else None
-                ),
-            }
-            for p in metax_dataset["provenance"]
-        ],
-        "metadata_owner": (
-            {
-                # TODO: `metadata_owner.user` and
-                # `metadata_owner.organization` might be hidden in some
-                # datasets, so they then set to `None`. Does it make
-                # sense fetch those datasets? Can it be avoided?
-                "organization": (
-                    metax_dataset["metadata_owner"].get("organization")
-                ),
-                "user": metax_dataset["metadata_owner"].get("user"),
-            }
-        ),
+        "provenance": provenance,
+        "metadata_owner": metadata_owner,
         "data_catalog": metax_dataset["data_catalog"],
     }
 
 
-def _map_actors(actors):
+def _map_actors(actors: Sequence[MetaxActor]) -> list[MetaxActor]:
     """A helper method for mapping actors in a dataset."""
-    return [
-        {
-            "roles": actor["roles"] if actor.get("roles") else [],
-            "person": (
-                {
-                    "name": actor["person"]["name"],
-                    "external_identifier": actor["person"][
-                        "external_identifier"
-                    ],
-                    # TODO: `email` might be hidden in some datasets, so
-                    # it is then set to `None`. Does it make sense fetch
-                    # those datasets? Can it be avoided?
-                    "email": actor["person"].get("email"),
-                }
-                if actor.get("person") is not None
-                else None
-            ),
-            "organization": (
-                {
-                    "pref_label": actor["organization"]["pref_label"],
-                    "url": actor["organization"]["url"],
-                    "external_identifier": actor["organization"][
-                        "external_identifier"
-                    ],
-                    "parent": (
-                        {
-                            "pref_label": actor["organization"]["parent"][
-                                "pref_label"
-                            ]
-                        }
-                        if actor["organization"]["parent"] is not None
-                        else None
-                    ),
-                }
-                if actor.get("organization") is not None
-                else None
-            ),
+
+    def _map_actor(actor: MetaxActor) -> MetaxActor:
+        act: MetaxActor = {
+            "roles": actor.get("roles", []),
+            "person": None,
+            "organization": None,
         }
-        for actor in actors
-    ]
+
+        if (person := actor.get("person")) is not None:
+            act["person"] = {
+                "name": person["name"],
+                "external_identifier": person["external_identifier"],
+                # TODO: `email` might be hidden in some datasets, so
+                # it is then set to `None`. Does it make sense fetch
+                # those datasets? Can it be avoided?
+                "email": person.get("email"),
+            }
+
+        if (org := actor.get("organization")) is not None:
+            parent: MetaxDataFieldBase | None = None
+            if org["parent"] is not None:
+                parent = {"pref_label": org["parent"]["pref_label"]}
+
+            act["organization"] = {
+                "pref_label": org["pref_label"],
+                "url": org["url"],
+                "external_identifier": org["external_identifier"],
+                "parent": parent,
+            }
+
+        return act
+
+    return [_map_actor(actor) for actor in actors]
